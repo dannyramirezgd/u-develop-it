65557aa11b3c846b913c52963bfda1d09bf99ad8
dannyrgd <danny.ramirezgd@gmail.com>
2022-02-07 17:22:22 -0500

    added query response

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username
+        user: 'root',
+        //Your MySQL password
+        password:'Tesoro0716!',
+        database:'election'
+    },
+    console.log('Connected to the election database')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //default response for any other request (NOT FOUND)  
 app.use((req,res)=> {
